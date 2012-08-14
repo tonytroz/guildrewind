aa194e5c910dbc8ab481b07ec756bcb1bfa32287
Tony Trozzo <tonytroz@gmail.com>
2012-08-14 18:23:11 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guildrewind::Application.config.secret_token = '72d410a3c975992eaa201efab1ba45219a9663d17cf4ab9e7b9dcc07eba4ad3c50e3dbb3e595e2381f144e96ac50673c47c48a50e487a47eec09959c59bb2d63'
