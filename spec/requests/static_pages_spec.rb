dee5a58cd67e9a74643d7ccce2cf137e33e09912
Tony Trozzo <tonytroz@gmail.com>
2012-08-14 21:10:07 -0400

    Add a full_title helper.

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  let(:base_title) { "GuildRewind" }
+
+  describe "Home page" do
+
+    it "should have the h1 'A Social Network For Lost Gamers'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'A Social Network For Lost Gamers')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => base_title)
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "#{base_title} | About Us")
+    end
+  end
+end
