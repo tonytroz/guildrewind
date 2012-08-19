6b305dc1d81aaa0e9e3fcf24e133de9c97f62573
Tony Trozzo <tonytroz@gmail.com>
2012-08-18 20:59:54 -0400

    Added user posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,9 +39,8 @@ class PostsController < ApplicationController
   def update
   	def update
     if @post.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
-      sign_in @user
-      redirect_to @user
+      flash[:success] = "Post updated"
+      redirect_to @post
     else
       render 'edit'
     end
