ef8302cfff5a716514aac2f980955eebd38be9c9
Tony Trozzo <tonytroz@gmail.com>
2012-08-24 19:14:20 -0400

    Updated config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,9 @@ Guildrewind::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  # For Heroku
+  config.assets.initialize_on_precompile = false
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
