25e94de1ceba5e3c2198c4eddddd65ec2868a892
Tony Trozzo <tonytroz@gmail.com>
2012-08-27 11:55:30 -0400

    Added scheduler task, updated css, added strip_tags to post, fixed post/user relationship, and updated views.

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,13 +9,28 @@
 #  updated_at :datetime         not null
 #
 
+include ActionView::Helpers::SanitizeHelper
+
 class Post < ActiveRecord::Base
   attr_accessible :content
   
   belongs_to :user
 
+  before_save :strip_html
+
   validates :content, presence: true, length: { maximum: 30000 }
   validates :user_id, presence: true
 
   default_scope order: 'posts.created_at DESC'
+
+  private
+    def strip_html # Automatically strips any tags from any string to text typed column  
+      for column in Post.content_columns  
+        if column.type == :string || column.type == :text # if the column is text-typed  
+          if !self[column.name].nil? # strip html from string if it's not empty  
+             self[column.name] = strip_tags(self[column.name])  
+          end  
+        end  
+      end  
+    end   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :post
+  has_many :posts
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
