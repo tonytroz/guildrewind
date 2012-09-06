75550576052a6055ba4ad481ecae620adaab33b3
Tony Trozzo <tonytroz@gmail.com>
2012-09-06 16:43:09 -0400

    Added validations to Tweet model.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,9 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :content, :url
 
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :url, presence: true
+
   def self.random
     Tweet.all(order: "RANDOM()")
   end
